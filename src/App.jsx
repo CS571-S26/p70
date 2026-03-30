f1c23a295392fa5588179580687f069124b9d4d2
Haoran Lin <hlin372@wisc.edu>
2026-03-29 21:40:34 -0500

    Set up routing for two pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
-import AboutPage from './pages/AboutPage'
-import ExplorePage from './pages/ExplorePage'
+import PageLayout from './components/PageLayout'
 import HomePage from './pages/HomePage'
-import SettingsPage from './pages/SettingsPage'
 import VisualizerPage from './pages/VisualizerPage'
+
+function PlaceholderPage({ title }) {
+  return (
+    <PageLayout title={title}>
+      <p className="text-muted mb-0">This page is a Phase B placeholder.</p>
+    </PageLayout>
+  )
+}
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/explore" element={<ExplorePage />} />
       <Route path="/visualizer" element={<VisualizerPage />} />
-      <Route path="/settings" element={<SettingsPage />} />
-      <Route path="/about" element={<AboutPage />} />
+      <Route path="/explore" element={<PlaceholderPage title="Explore" />} />
+      <Route path="/settings" element={<PlaceholderPage title="Settings" />} />
+      <Route path="/about" element={<PlaceholderPage title="About" />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
